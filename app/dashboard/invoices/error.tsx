8c8f6f6d847ec921076047e9ceade884e25d5f45
Jhonathan Velez <kokoa.arte@gmail.com>
2025-07-10 10:04:54 -0500

    update commit

--- app/dashboard/invoices/error.tsx
+++ app/dashboard/invoices/error.tsx
@@ -9,10 +9,8 @@ export default function Error({error,reset,}:{error: Error & { digest?: string }
       <button
         className="mt-4 rounded-md bg-blue-500 px-4 py-2 text-sm text-white transition-colors hover:bg-blue-400"
         onClick={
-          // Attempt to recover by trying to re-render the invoices route
           () => reset()
-        }
-      >
+        }>
         Try again
       </button>
     </main>
